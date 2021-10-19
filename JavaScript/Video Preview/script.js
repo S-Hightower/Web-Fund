8a186115192f483573ec66d4e4766311f0572244
Shawna <sdhightower6@gmail.com>
2021-10-18 18:34:24 -0600

    init commit

---
+++ JavaScript/Video Preview/script.js
@@ -0,0 +1,10 @@
+console.log("page loaded...");
+
+function playVideo(element) {
+    element.classList.play("video");    
+}
+    
+function pauseVideo(element) {
+    element.classList.pause("video");    
+}
+
